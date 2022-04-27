8e3b8b5d2e837cdfb7c71dbd4e4a646fc221f078
Lucas Grass <grassberaldo@gmail.com>
2022-04-26 21:23:31 -0300

    inicio da tela de login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import SignIn from './login'
 
 const Home: NextPage = () => {
   return (
@@ -12,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <h1>Hello World</h1>
+        <SignIn/>
       </main>
     </div>
   )
