8e5f9a59f456f9a981a3988cd9971b0a24a37e1b
Lucas Grass <grassberaldo@gmail.com>
2022-05-03 10:50:22 -0300

    tela de recuperação de senha

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {AppProps} from 'next/app'
-import './login/style.css'
-
-function MyApp ({ Component, pageProps }:AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
