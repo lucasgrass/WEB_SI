e65d79ea85ae0e5576fbbaaaf8bfac8d2a36e483
Lucas Grass <grassberaldo@gmail.com>
2022-06-06 10:47:42 -0300

    alguns detalhes

---
+++ src/mocks/userList.ts
@@ -0,0 +1,28 @@
+import { UserProps } from '../models/module';
+
+export const UserList = [
+  {
+    id: '589f54d3-5410-4756-9e68-6265dbe38273',
+    title: 'vitorferrara@hotmail.com',
+    name: 'Vitor',
+    password: '123123',
+    profiletype: 0,
+    updateAt: '2022-04-01T14:47:45.157Z',
+  },
+  {
+    id: '589f54d3-5410-4756-9e68-6265dbe38288',
+    email: 'teste@gmail.com',
+    name: 'Lucas',
+    password: '123123',
+    profiletype: 0,
+    updateAt: '2022-04-01T14:47:45.157Z',
+  },
+  {
+    id: '589f54d3-5410-4756-9e68-6265dbe38299',
+    title: 'test2@gmail.com',
+    name: 'Guilherme',
+    password: '123123',
+    profiletype: 0,
+    updateAt: '2022-04-01T14:47:45.157Z',
+  },
+] as UserProps[];
