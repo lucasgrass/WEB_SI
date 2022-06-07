7eb8d40fc6b270a0c7f7a7547c4d4f5540aa6aed
404renato <404renato@gmail.com>
2022-06-06 22:28:11 -0300

    map

--- src/pages/dashboard/map_occurrences.tsx
+++ src/pages/dashboard/map_occurrences.tsx
@@ -1,42 +1,104 @@
 import Sidebar from '@portal/components/Sidebar';
-import mapboxgl from 'mapbox-gl';
 import { NextPage } from 'next';
-import React, { useEffect, useRef, useState } from 'react';
-
-mapboxgl.accessToken =
+import React, { useMemo, useState } from 'react';
+import Map, { Marker, Popup } from 'react-map-gl';
+import CITIES from '../../../CITIES.json';
+import Pin from './pin';
+const TOKEN =
   'pk.eyJ1IjoicmVuYXRvYmFyYmFzYW50b3MiLCJhIjoiY2wzdzA2NmhwMDFuNTNjbjFvamQ0Y2xtaSJ9.dcZxm0l1Qe3_Cqzx9haXkw';
 
 const Mapa: NextPage = () => {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-47.05265);
-  const [lat, setLat] = useState(-22.83438);
-  const [zoom, setZoom] = useState(15);
+  const [popupInfo, setPopupInfo] = useState(null);
 
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom,
-    });
-
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-  }, []);
+  const pins = useMemo(
+    () =>
+      CITIES.features.map((city, index) => (
+        <Marker
+          key={`marker-${index}`}
+          longitude={city.geometry.coordinates[0]}
+          latitude={city.geometry.coordinates[1]}
+          anchor="bottom"
+          style={{
+            width: '20px',
+            height: '20px',
+            bottom: '600px',
+            position: 'fixed',
+          }}
+          onClick={(e) => {
+            console.log(city);
+            // If we let the click event propagates to the map, it will immediately close the popup
+            // with `closeOnClick: true`
+            e.originalEvent.stopPropagation();
+            setPopupInfo(city);
+          }}
+        >
+          <Pin />
+        </Marker>
+      )),
+    []
+  );
 
   return (
     <div className="container-sidebar">
       <Sidebar />
+
       <div className="container-middle">
-        <div className="map-info">
-          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-        </div>
-        <div ref={mapContainer} className="map-container" />
+        <Map
+          initialViewState={{
+            latitude: -22.83,
+            longitude: -47.05,
+            zoom: 13,
+            bearing: 0,
+            pitch: 0,
+          }}
+          mapStyle="mapbox://styles/mapbox/streets-v11"
+          mapboxAccessToken={TOKEN}
+          style={{ width: '100%', height: '100%' }}
+          attributionControl={false}
+        >
+          {pins}
+
+          {popupInfo && (
+            <Popup
+              anchor="top"
+              longitude={Number(popupInfo.geometry.coordinates[0])}
+              latitude={Number(popupInfo.geometry.coordinates[1])}
+              onClose={() => setPopupInfo(null)}
+              style={{ bottom: '600px', position: 'fixed' }}
+            >
+              <div
+                style={{
+                  width: '250px',
+                  height: '100px',
+                  backgroundColor: 'white',
+                  borderRadius: 16,
+                }}
+              >
+                <div
+                  style={{
+                    textAlign: 'center',
+                    marginBottom: '10px',
+                  }}
+                >
+                  <h4 style={{ fontWeight: 'bold' }}>Nome Ocorrencia</h4>
+                </div>
+
+                <div style={{ marginLeft: '5px' }}>
+                  <p style={{ fontWeight: 'bold', marginBottom: '5px' }}>
+                    Tipo:
+                    <span style={{ fontWeight: 'normal' }}> Crime</span>
+                  </p>
+                  <p style={{ fontWeight: 'bold' }}>
+                    Subtipo:
+                    <span style={{ fontWeight: 'normal' }}> Muito Crime</span>
+                  </p>
+                </div>
+
+                <img width="100%" src={popupInfo.image} />
+              </div>
+            </Popup>
+          )}
+        </Map>
       </div>
     </div>
   );
